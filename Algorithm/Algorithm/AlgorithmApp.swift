0bc4d41e1d189fa7bd5e6de957efd574924e0d6e
837100 <shtjdrud0830@naver.com>
2025-02-23 13:24:49 +0900

    알고리즘 공부 프로젝트 생성

---
+++ Algorithm/Algorithm/AlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmApp.swift
+//  Algorithm
+//
+//  Created by NO SEONGGYEONG on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Algorithm/Algorithm/Preview Content/Knou/Chapter1/Ex1_1.swift
@@ -0,0 +1,9 @@
+//
+//  Ex1_1.swift
+//  Algorithm
+//
+//  Created by NO SEONGGYEONG on 2/23/25.
+//
+
+import Foundation
+
