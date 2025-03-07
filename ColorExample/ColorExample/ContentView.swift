cd5ece4486c0f8c3ae3a84faf83c7a97ba5ad831
837100 <shtjdrud0830@naver.com>
2025-03-07 23:42:12 +0900

    컬러 실습 코드

---
+++ ColorExample/ColorExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorExample
+//
+//  Created by NO SEONGGYEONG on 3/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
